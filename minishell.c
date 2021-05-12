3819a5482841519a378eac3410f3e40093ea7b49
Badrien19 <adrienbaar@hotmail.com>
2021-05-12 16:13:59 +0200

    normv3 for minishell.c

--- minishell.c
+++ minishell.c
@@ -9,13 +9,14 @@ char	*stradd(char *s1, char const *s2)
 	size_t	size_s1;
 	size_t	size_s2;
 
-    size_s1 = 0;
-    if (s1 != NULL)
-	    size_s1 = ft_strlen(s1);
+	size_s1 = 0;
+	if (s1 != NULL)
+		size_s1 = ft_strlen(s1);
 	size_s2 = ft_strlen(s2);
 	if (s1 == NULL && s2 == NULL)
 		return (NULL);
-	if (!(ret = malloc((sizeof(char) * (size_s1 + size_s2) + 1))))
+	ret = malloc((sizeof(char) * (size_s1 + size_s2) + 1));
+	if (ret == NULL)
 		return (NULL);
 	if (s1 != NULL)
 	{
@@ -28,37 +29,39 @@ char	*stradd(char *s1, char const *s2)
 	return (ret);
 }
 
-char    *sh_read_line()
+char	*sh_read_line(void)
 {
-    char    buff[SH_LINE_BUFFSIZE + 1];
-    char    *str;
-    size_t  ret;
+	char	buff[SH_LINE_BUFFSIZE + 1];
+	char	*str;
+	size_t	ret;
 
-    str = NULL;
-    while (1)
-    {
-        ret = read(1, buff, SH_LINE_BUFFSIZE);
-        buff[ret] = 0;
-        str = stradd(str, buff);
-        if (ft_strchr(buff, '\n') != NULL)
-            break;
-    }
-    return (str);
+	str = NULL;
+	while (1)
+	{
+		ret = read(1, buff, SH_LINE_BUFFSIZE);
+		buff[ret] = 0;
+		str = stradd(str, buff);
+		if (ft_strchr(buff, '\n') != NULL)
+			break ;
+	}
+	return (str);
 }
 
-void    sh_pre()
+void	sh_pre(void)
 {
-    write(1, "\033[1;32m", 7);
-    write(1, "minishell > ", 13);
-    write(1, "\033[0m", 4);
+	write(1, "\033[1;32m", 7);
+	write(1, "minishell > ", 13);
+	write(1, "\033[0m", 4);
 }
 
-int main(int argc, char const *argv[])
+int	main(int argc, char const *argv[])
 {
-    while (1)
-    {
-        sh_pre();
-        sh_read_line();
-    }
-    return (0);
+	char	*cmd;
+
+	while (1)
+	{
+		sh_pre();
+		cmd = sh_read_line();
+	}
+	return (0);
 }
