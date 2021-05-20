9503af8b384b05daa4de1003b36097a22ccc73ce
arapaill <arapaill@student.s19.be>
2021-05-20 22:44:53 +0200

    making echo work

--- hub.c
+++ hub.c
@@ -6,47 +6,60 @@
 /*   By: user42 <user42@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/05/17 17:05:31 by user42            #+#    #+#             */
-/*   Updated: 2021/05/20 18:49:37 by user42           ###   ########.fr       */
+/*   Updated: 2021/05/20 22:43:13 by user42           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_minishell.h"
+
+void	print_quote(void *s)
+{
+	char	*str;
+	int		i;
+
+	str = (char *)s;
+	i = 0;
+	while(i < (ft_strlen(str) - 2))
+		printf("%c", str[++i]);
+}
 
 void	cmd_echo(t_list *list)
 {
 	int		quote;
 
 	quote = 0;
-	while (list && list->content->type != semicolon)
+	while (list && list->content->type != 3)
 	{
-		if(list->content->type == single_quote)
+		if(list->content->type == 9)
 			quote = 1;
-			while (quote = 1)
+			if (quote == 1)
 			{
-				printf("%s", list->content->value);
+				print_quote(list->content->value);
 				if(list->next)
 					list = list->next;
 				else
 					return ;
-				if(list->content->type == semicolon)
+				if(list->content->type == 3 || list->content->type == 1)
 					return ;
-				if(list->content->type == single_quote)
-					quote = 0;
+				quote = 0;
 			}
-			if(list->content->type == double_quote)
+			if(list->content->type == 10)
 				quote = 1;
-			while (quote = 1)
+			if (quote == 1)
 			{
-				printf("%s", list->content->value);
-				list = list->next;
-				if(list->content->type == semicolon)
+				print_quote(list->content->value);
+				if(list->next)
+					list = list->next;
+				else
 					return ;
-				if(list->content->type == double_quote)
-					quote = 0;
+				if(list->content->type == 3 || list->content->type == 1)
+					return ;
+				quote = 0;
 			}
-			if(list->content->type == semicolon)
+			if(list->content->type == 3 || list->content->type == 1)
 				return ;
-			if(list->content->type == backslash )
+
+			if(list->content->type == 11 )
 				{
 					if(list->next)
 						printf("%s", list->next->content->value);
@@ -55,7 +68,7 @@ void	cmd_echo(t_list *list)
 					else
 						return ;
 				}
-			if(list->content->type == semicolon)
+			if(list->content->type == 3 || list->content->type == 1)
 				return ;
 			printf("%s", list->content->value);
 			if(list->next)
@@ -77,7 +90,7 @@ void	cmd_hub(void)
 		if(list->content->type == literal && !ft_strcmp(list->content->value, "exit"))
 			error("exit minishell\n");
 		if(list->content->type == literal && !ft_strcmp(list->content->value, "echo"))
-			cmd_echo(list->next);
+			cmd_echo(list->next->next);
 		if(list->next)
 			list = list->next;
 		else
