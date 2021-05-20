34a2e076f1f7912dc592438d09a978f22738b7a5
CG-Walker <cogo97@hotmail.com>
2021-05-20 17:49:22 +0200

    debug plus clair

--- debug.c
+++ debug.c
@@ -10,10 +10,10 @@ void    print_list(void)
 	{
 		while (tmp_sys_infos.list_input->next != NULL)
 		{
-			printf("'%s' (%i) -> %p\n", (char*)tmp_sys_infos.list_input->content->value, tmp_sys_infos.list_input->content->type, tmp_sys_infos.list_input->next);
+			printf("%-20s (%2i) -> %18p\n", (char*)tmp_sys_infos.list_input->content->value, tmp_sys_infos.list_input->content->type, tmp_sys_infos.list_input->next);
 			tmp_sys_infos.list_input = tmp_sys_infos.list_input->next;
 		}
-		printf("'%s' (%i) -> %p\n", (char*)tmp_sys_infos.list_input->content->value, tmp_sys_infos.list_input->content->type, tmp_sys_infos.list_input->next);
+		//printf("'%-10s' (%2i) -> %25p\n", (char*)tmp_sys_infos.list_input->content->value, tmp_sys_infos.list_input->content->type, tmp_sys_infos.list_input->next);
 	}
 	printf("-----------------------------------------------\n");
 }
