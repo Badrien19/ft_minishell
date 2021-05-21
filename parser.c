d79fffd573e7108abfc2498f4deb5fec33289838
CG-Walker <cogo97@hotmail.com>
2021-05-21 15:27:46 +0200

    Gère les backslash dans concat_tokens_quotes

--- parser.c
+++ parser.c
@@ -115,39 +115,59 @@ void *join_two_tokens(t_token *token_1, t_token *token_2)
     return (token);
 }
 
-void    del_node(t_list *node)
+void    clear_node(t_list *node)
 {
     node->content = NULL;
 	node->next = NULL;
 }
 
-void    arrange_list()
+void    relink_nodes()
 {
     t_list *tmp_list;
 
     g_sys_infos.list_input->content = join_two_tokens(g_sys_infos.list_input->content, g_sys_infos.list_input->next->content);
+    printf("relink : %s\n", g_sys_infos.list_input->content->value);
     tmp_list = g_sys_infos.list_input->next->next;
-    del_node(g_sys_infos.list_input->next);
+    clear_node(g_sys_infos.list_input->next);
     g_sys_infos.list_input->next->content = tmp_list->content;
     g_sys_infos.list_input->next->next = tmp_list->next;
+}
+
+char    get_last_char(void *value)
+{
+    size_t size;
+    char *str;
+    char ret;
+
+    str = (char*)value;
+    size = ft_strlen(str);
+    ret = str[size - 1];
+
+    return (ret);
 }
 
 void    concat_tokens_quotes()
 {
     t_list *begin;
     t_token_type quote_type;
+    char *tmp_value;
 
     begin = g_sys_infos.list_input;
     if (checking_if_quotes_even() == False)
         return ;
     while (g_sys_infos.list_input != NULL)
     {
-        if (g_sys_infos.list_input->content->type == single_quote || g_sys_infos.list_input->content->type == double_quote)
+        if (get_token_type(g_sys_infos.list_input->content) == single_quote || get_token_type(g_sys_infos.list_input->content) == double_quote)
         {
-            quote_type = g_sys_infos.list_input->content->type;
-            while (g_sys_infos.list_input->next->content->type != quote_type && g_sys_infos.list_input->next != NULL)
-                arrange_list();
-            arrange_list();
+            quote_type = get_token_type(g_sys_infos.list_input->content);
+            while ((get_token_type(g_sys_infos.list_input->next->content) != quote_type && g_sys_infos.list_input->next != NULL) || 
+            (get_last_char(get_token_value(g_sys_infos.list_input->content)) == '\\' && get_token_type(g_sys_infos.list_input->next->content) == quote_type))
+            {
+                //printf("[in] last : %c -> %i\n", get_last_char(get_token_value(g_sys_infos.list_input->content)), get_token_type(g_sys_infos.list_input->next->content));
+                relink_nodes();
+            }
+            //printf("[out] last : %c -> %i\n", get_last_char(get_token_value(g_sys_infos.list_input->content)), get_token_type(g_sys_infos.list_input->next->content));
+            relink_nodes();
         }
         g_sys_infos.list_input = g_sys_infos.list_input->next;
     }
@@ -165,15 +185,9 @@ void    concat_tokens_all()
     {
         //printf("'%s' -> '%s'\n", g_sys_infos.list_input->content->value, g_sys_infos.list_input->next->content->value);
         if (get_token_type(g_sys_infos.list_input->content) == get_token_type(g_sys_infos.list_input->next->content))
-        {
-            //printf("[if]\n");
-            arrange_list();
-        }
+            relink_nodes();
         else
-        {
-            //printf("[else]\n");
             g_sys_infos.list_input = g_sys_infos.list_input->next;
-        }
     }
     g_sys_infos.list_input = begin;
 }
