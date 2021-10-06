734b9bd457730a355f76d73328717bd7e73efa0f
CG-Walker <cogo97@hotmail.com>
2021-10-06 15:16:54 +0200

    Ajout de fonction pour récuperer le prochain litteral et le prochain/précédent cmd_instr

---
+++ srcs/parsing/search_in_parsing.c
@@ -0,0 +1,52 @@
+#include "../../includes/ft_minishell.h"
+
+t_cmd	*find_next_cmd(void)
+{
+	t_cmd *current;
+	t_cmd *ret;
+
+	current = g_minishell.list_input;
+	while (g_minishell.list_input && g_minishell.list_input->content->type != cmd_instr
+	&& g_minishell.list_input->content->type != pipeline)
+		g_minishell.list_input = g_minishell.list_input->next;
+	if (g_minishell.list_input && g_minishell.list_input->content->type == pipeline)
+		ret = NULL;
+	else
+		ret = g_minishell.list_input;
+	g_minishell.list_input = current;
+	return (ret);
+}
+
+t_cmd	*find_prev_cmd(void)
+{
+	t_cmd *current;
+	t_cmd *ret;
+
+	current = g_minishell.list_input;
+	while (g_minishell.list_input && g_minishell.list_input->content->type != cmd_instr
+	&& g_minishell.list_input->content->type != pipeline)
+		g_minishell.list_input = g_minishell.list_input->prev;
+	if (g_minishell.list_input && g_minishell.list_input->content->type == pipeline)
+		ret = NULL;
+	else
+		ret = g_minishell.list_input;
+	g_minishell.list_input = current;
+	return (ret);
+}
+
+t_cmd	*find_next_literal(void)
+{
+	t_cmd *current;
+	t_cmd *ret;
+
+	current = g_minishell.list_input;
+	while (g_minishell.list_input && g_minishell.list_input->content->type != literal
+	&& g_minishell.list_input->content->type != pipeline)
+		g_minishell.list_input = g_minishell.list_input->next;
+	if (g_minishell.list_input && g_minishell.list_input->content->type == pipeline)
+		ret = NULL;
+	else
+		ret = g_minishell.list_input;
+	g_minishell.list_input = current;
+	return (ret);
+}
