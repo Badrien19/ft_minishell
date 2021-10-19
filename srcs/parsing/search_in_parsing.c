4f48027d017bad739a78ef470befcf371724493d
CG-Walker <cogo97@hotmail.com>
2021-10-19 15:34:20 +0200

    Normed (Et c'est dégueulasse...)

--- srcs/parsing/search_in_parsing.c
+++ srcs/parsing/search_in_parsing.c
@@ -1,22 +1,37 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   search_in_parsing.c                                :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: cgoncalv <cgoncalv@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/10/19 15:10:50 by cgoncalv          #+#    #+#             */
+/*   Updated: 2021/10/19 15:10:50 by cgoncalv         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "../../includes/ft_minishell.h"
 
 /* find_next_cmd(void)
-** Renvoie un pointeur vers le prochain cmd_instr. Renvoie NULL s'il ne trouve rien.
-** S'arrête automatiquement dès qu'il trouve un '|', un ';' ou la fin de la chaine.
+** Renvoie un pointeur vers le prochain cmd_instr. 
+** Renvoie NULL s'il ne trouve rien.
+** S'arrête automatiquement dès qu'il trouve un '|', 
+** un ';' ou la fin de la chaine.
 */
 t_cmd	*find_next_cmd(void)
 {
-	t_cmd *current;
-	t_cmd *ret;
+	t_cmd	*current;
+	t_cmd	*ret;
 
 	current = g_minishell.list_input;
 	g_minishell.list_input = g_minishell.list_input->next;
-	while (g_minishell.list_input 
-	&& g_minishell.list_input->content->type != cmd_instr
-	&& g_minishell.list_input->content->type != pipeline
-	&& g_minishell.list_input->content->type != semicolon)
+	while (g_minishell.list_input
+		&& g_minishell.list_input->content->type != cmd_instr
+		&& g_minishell.list_input->content->type != pipeline
+		&& g_minishell.list_input->content->type != semicolon)
 		g_minishell.list_input = g_minishell.list_input->next;
-	if (g_minishell.list_input && g_minishell.list_input->content->type == cmd_instr)
+	if (g_minishell.list_input
+		&& g_minishell.list_input->content->type == cmd_instr)
 		ret = g_minishell.list_input;
 	else
 		ret = NULL;
@@ -25,22 +40,25 @@ t_cmd	*find_next_cmd(void)
 }
 
 /* find_next_cmd(void)
-** Renvoie un pointeur vers le précédent cmd_instr. Renvoie NULL s'il ne trouve rien.
-** S'arrête automatiquement dès qu'il trouve un '|', un ';' ou la fin de la chaine.
+** Renvoie un pointeur vers le précédent cmd_instr. 
+** Renvoie NULL s'il ne trouve rien.
+** S'arrête automatiquement dès qu'il trouve un '|',
+** un ';' ou la fin de la chaine.
 */
 t_cmd	*find_prev_cmd(void)
 {
-	t_cmd *current;
-	t_cmd *ret;
+	t_cmd	*current;
+	t_cmd	*ret;
 
 	current = g_minishell.list_input;
 	g_minishell.list_input = g_minishell.list_input->prev;
-	while (g_minishell.list_input 
-	&& g_minishell.list_input->content->type != cmd_instr
-	&& g_minishell.list_input->content->type != pipeline
-	&& g_minishell.list_input->content->type != semicolon)
+	while (g_minishell.list_input
+		&& g_minishell.list_input->content->type != cmd_instr
+		&& g_minishell.list_input->content->type != pipeline
+		&& g_minishell.list_input->content->type != semicolon)
 		g_minishell.list_input = g_minishell.list_input->prev;
-	if (g_minishell.list_input && g_minishell.list_input->content->type == cmd_instr)
+	if (g_minishell.list_input
+		&& g_minishell.list_input->content->type == cmd_instr)
 		ret = g_minishell.list_input;
 	else
 		ret = NULL;
@@ -49,27 +67,31 @@ t_cmd	*find_prev_cmd(void)
 }
 
 /* find_next_literal(size_t range)
-** range : Jusqu'à combien de node (en ignorant les espaces) chercher le literal. 0 = pas de limite.
-** S'arrête automatiquement dès qu'il trouve un '|' ou la fin de la chaine.
+** range : Jusqu'à combien de node (en ignorant les espaces) 
+** chercher le literal. 0 = pas de limite.
+** S'arrête automatiquement dès qu'il trouve
+** un '|' ou la fin de la chaine.
 */
 t_cmd	*find_next_literal(int range)
 {
-	t_cmd *current;
-	t_cmd *ret;
+	t_cmd	*current;
+	t_cmd	*ret;
 
 	if (range == 0)
 		range = ft_cmdsize(g_minishell.list_input);
 	else
 		range++;
 	current = g_minishell.list_input;
-	while (g_minishell.list_input && g_minishell.list_input->content->type != literal
-	&& g_minishell.list_input->content->type != pipeline && range > 0)
+	while (g_minishell.list_input
+		&& g_minishell.list_input->content->type != literal
+		&& g_minishell.list_input->content->type != pipeline && range > 0)
 	{
 		if (g_minishell.list_input->content->type != space)
 			range--;
 		g_minishell.list_input = g_minishell.list_input->next;
 	}
-	if ((g_minishell.list_input && g_minishell.list_input->content->type == pipeline) || range <= 0)
+	if ((g_minishell.list_input
+			&& g_minishell.list_input->content->type == pipeline) || range <= 0)
 		ret = NULL;
 	else
 		ret = g_minishell.list_input;
--- srcs/tokens/utils_concat_tokens.c
+++ srcs/tokens/utils_concat_tokens.c
@@ -1,55 +1,62 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   utils_concat_tokens.c                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: cgoncalv <cgoncalv@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/10/19 15:29:55 by cgoncalv          #+#    #+#             */
+/*   Updated: 2021/10/19 15:29:55 by cgoncalv         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "../../includes/ft_minishell.h"
 
-void *join_two_tokens(t_token *token_1, t_token *token_2) // Free
+void	*join_two_tokens(t_token *token_1, t_token *token_2)
 {
-    char *str_1;
-    char *str_2;
-    char *new_tok_value;
-    t_token *token;
+	char	*str_1;
+	char	*str_2;
+	char	*new_tok_value;
+	t_token	*token;
 
-    str_1 = token_1->value;
-    str_2 = token_2->value;
-    //printf("%s - %s\n", str_1, str_2);
-    new_tok_value = ft_strjoin(str_1, str_2);
-    token = create_token(new_tok_value, get_token_type(token_1));
-    //printf("%s\n", token->value);
-    free(token_1->value);
-    free(token_1);
-    return (token);
+	str_1 = token_1->value;
+	str_2 = token_2->value;
+	new_tok_value = ft_strjoin(str_1, str_2);
+	token = create_token(new_tok_value, get_token_type(token_1));
+	free(token_1->value);
+	free(token_1);
+	return (token);
 }
 
-static void clear_node(t_cmd *node)
+static void	clear_node(t_cmd *node)
 {
-    node->content = NULL;
-    free(node->content);
+	node->content = NULL;
+	free(node->content);
 	node->next = NULL;
-    free(node->next);
-    free(node);
+	free(node->next);
+	free(node);
 }
 
-/* Fusionne le token actuel avec le token suivant et garde le type du token actuel. */
-void    relink_nodes()
+/* Fusionne le token actuel avec le token suivant 
+** et garde le type du token actuel. 
+*/
+void	relink_nodes(void)
 {
-    t_cmd *tmp_list;
+	t_cmd	*tmp_list;
 
-    tmp_list = NULL;
-    g_minishell.list_input->content = join_two_tokens(g_minishell.list_input->content, g_minishell.list_input->next->content);
-    //printf("relink : %s\n", g_minishell.list_input->content->value);
-    if (g_minishell.list_input->next->next)
-        tmp_list = g_minishell.list_input->next->next;
-    else
-    {
-    /*  tmp_list = malloc(sizeof(t_list));
-        if (!(tmp_list))
-            exit(-1);
-        tmp_list->content = create_token(NULL, 0);
-        tmp_list->next = NULL; */
-        clear_node(g_minishell.list_input->next);
-        g_minishell.list_input->next = NULL;
-    }
-    if (tmp_list)
-    {
-        g_minishell.list_input->next = tmp_list;
-    }
-    //print_current_chain();
+	tmp_list = NULL;
+	g_minishell.list_input->content = join_two_tokens(
+			g_minishell.list_input->content,
+			g_minishell.list_input->next->content);
+	if (g_minishell.list_input->next->next)
+		tmp_list = g_minishell.list_input->next->next;
+	else
+	{
+		clear_node(g_minishell.list_input->next);
+		g_minishell.list_input->next = NULL;
+	}
+	if (tmp_list)
+	{
+		g_minishell.list_input->next = tmp_list;
+	}
 }
