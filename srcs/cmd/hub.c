853367e047853d71237b094300efb5d8ef702738
CG-Walker <cogo97@hotmail.com>
2021-12-14 14:41:26 +0100

    CHANGE : Réécriture totale du hub.

--- srcs/cmd/hub.c
+++ srcs/cmd/hub.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   hub.c                                              :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: user42 <user42@student.42.fr>              +#+  +:+       +#+        */
+/*   By: cgoncalv <cgoncalv@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/08/31 15:15:04 by user42            #+#    #+#             */
-/*   Updated: 2021/12/13 16:03:55 by user42           ###   ########.fr       */
+/*   Updated: 2021/12/14 14:40:55 by cgoncalv         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -36,43 +36,28 @@ static void	ft_switch(t_cmd *list)
 		cmd_execve(list);
 }
 
-static void	loop_hub(t_cmd *list)
-{
-	replace_value_from_env(list);
-	concat_tokens_same_type();
-	detect_cmd_type();
-	detect_file_type();
-	if (list && list->content->type == cmd_instr)
-		ft_switch(list);
-	else if (!ft_isstop(list) || list->content->type == filename)
-		return ;
-	else if (errno == 2)
-		parsing_error(MS_ERROR_NO_CMD);
-	else if (errno != 0)
-		perror("minishell");
-}
-
 void	cmd_hub(void)
 {
-	t_cmd	*list;
+	t_cmd *current;
 
-	list = g_minishell.list_input;
-	while (list->content->type == space && list->next)
-		list = list->next;
-	loop_hub(list);
-	while (list != NULL)
+	g_minishell.list_input = ft_cmdfirst(g_minishell.list_input);
+	while (g_minishell.list_input)
 	{
-		if (!ft_isstop(list))
-		{
-			while (list->content->type == space && list->next)
-				list = list->next;
-			while (list->next && list->content->type != cmd_instr
-				&& list->content->type != literal
-				&& list->content->type != filename
-				&& list->content->type != variable)
-				list = list->next;
-			loop_hub(list);
-		}
-		list = list->next;
+		replace_value_from_env(g_minishell.list_input);
+		concat_tokens_same_type();
+		current = g_minishell.list_input;
+		detect_cmd_type();
+		detect_file_type();
+		g_minishell.list_input = current;
+		if (find_next_cmd())
+			ft_switch(find_next_cmd());
+		else
+			ft_switch(find_next_literal(0));
+		while (g_minishell.list_input
+			&& g_minishell.list_input->content->type != semicolon
+			&& g_minishell.list_input->content->type != pipeline)
+			g_minishell.list_input = g_minishell.list_input->next;
+		if (g_minishell.list_input && g_minishell.list_input->next)
+			g_minishell.list_input = g_minishell.list_input->next;
 	}
 }
